9d79674a03c092653107003acf6e8c94e06dd10d
Duygu98 <77547532+Duygu98@users.noreply.github.com>
2021-01-21 17:48:07 +0300

    Add files via upload

---
+++ MyDictonary/MyDictinary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictinary
+    {
+
+        public MyDictinary()
+        {
+            Dictionary<int, string> ogrenci = new Dictionary<int, string>();
+            ogrenci.Add(123, "Duygu UĞUR");
+            ogrenci.Add(124, "Ahmet Yasar");
+            ogrenci.Add(125, "Samet Mutlu");
+            ogrenci.Add(126, "Fatma Kurt");
+            ogrenci.Add(127, "Furkan Bereket");
+            ogrenci.Add(128, "Beyza Yüksel");
+
+            foreach (var ogrenciler in ogrenci)
+            {
+                Console.WriteLine("Ogrenci No : " + ogrenciler.Key + "\t Adi-Soyadi :  " + ogrenciler.Value);
+            }
+        }
+           
+     }
+}
